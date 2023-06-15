5164e200de204f4b898985750f7a66bb50ccabbb
ALKHOS <53263505+ALKHOS71@users.noreply.github.com>
2023-06-15 13:36:48 +0200

    Add files via upload

---
+++ qgis2web_2023_06_15-12_14_35_916200/styles/POLE_5_style.js
@@ -0,0 +1,32 @@
+var size = 0;
+var placement = 'point';
+
+var style_POLE_5 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    var value = ""
+    var labelText = "";
+    size = 0;
+    var labelFont = "14.3px \'Arial\', sans-serif";
+    var labelFill = "#00008e";
+    var bufferColor = "#fafafa";
+    var bufferWidth = 3.5000000000000013;
+    var textAlign = "left";
+    var offsetX = 8;
+    var offsetY = 3;
+    var placement = 'point';
+    if (feature.get("Nom") !== null) {
+        labelText = String(feature.get("Nom"));
+    }
+    var style = [ new ol.style.Style({
+        image: new ol.style.RegularShape({radius: 5.86666 + size, points: 5,
+            radius2: 2.93333, stroke: new ol.style.Stroke({color: 'rgba(0,31,1,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}), fill: new ol.style.Fill({color: 'rgba(0,31,1,1.0)'})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+
+    return style;
+};
---
+++ qgis2web_2023_06_15-12_14_35_916200/styles/PROGRAMMEDESIGNALISATIONJUIN2023_6_style.js
@@ -0,0 +1,95 @@
+var size = 0;
+var placement = 'point';
+function categories_PROGRAMMEDESIGNALISATIONJUIN2023_6(feature, value, size, resolution, labelText,
+                       labelFont, labelFill, bufferColor, bufferWidth,
+                       placement) {
+                switch(value.toString()) {case 'Panneau image':
+                    return [ new ol.style.Style({
+        image: new ol.style.Circle({radius: 7.2 + size,
+            stroke: new ol.style.Stroke({color: 'rgba(128,67,36,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 1}), fill: new ol.style.Fill({color: 'rgba(172,91,49,1.0)'})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'Marquage au sol':
+                    return [ new ol.style.Style({
+        image: new ol.style.RegularShape({radius: 8.4 + size, points: 4,
+            stroke: new ol.style.Stroke({color: 'rgba(254,0,21,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 1}), fill: new ol.style.Fill({color: 'rgba(246,91,101,1.0)'})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'Totem "signal"':
+                    return [ new ol.style.Style({
+        image: new ol.style.RegularShape({radius: 9.2 + size, points: 6,
+            stroke: new ol.style.Stroke({color: 'rgba(87,219,255,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}), fill: new ol.style.Fill({color: 'rgba(1,39,255,1.0)'})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'Mini Totem "signal"':
+                    return [ new ol.style.Style({
+        image: new ol.style.RegularShape({radius: 9.2 + size, points: 6,
+            stroke: new ol.style.Stroke({color: 'rgba(1,56,255,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}), fill: new ol.style.Fill({color: 'rgba(97,205,255,1.0)'})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'Dv21a':
+                    return [ new ol.style.Style({
+        image: new ol.style.RegularShape({radius: 8.4 + size, points: 3,
+            stroke: new ol.style.Stroke({color: 'rgba(255,103,1,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 1}), fill: new ol.style.Fill({color: 'rgba(249,139,42,1.0)'})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'Dv42b':
+                    return [ new ol.style.Style({
+        image: new ol.style.RegularShape({radius: 8.4 + size, points: 3,
+            stroke: new ol.style.Stroke({color: 'rgba(128,17,25,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 1}), fill: new ol.style.Fill({color: 'rgba(219,30,42,1.0)'})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'Dv43a':
+                    return [ new ol.style.Style({
+        image: new ol.style.RegularShape({radius: 8.4 + size, points: 3,
+            stroke: new ol.style.Stroke({color: 'rgba(255,187,1,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 1}), fill: new ol.style.Fill({color: 'rgba(251,255,1,1.0)'})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;}};
+
+var style_PROGRAMMEDESIGNALISATIONJUIN2023_6 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    var value = feature.get("Type_panne");
+    var labelText = "";
+    size = 0;
+    var labelFont = "10px, sans-serif";
+    var labelFill = "#000000";
+    var bufferColor = "";
+    var bufferWidth = 0;
+    var textAlign = "left";
+    var offsetX = 8;
+    var offsetY = 3;
+    var placement = 'point';
+    if ("" !== null) {
+        labelText = String("");
+    }
+    
+var style = categories_PROGRAMMEDESIGNALISATIONJUIN2023_6(feature, value, size, resolution, labelText,
+                          labelFont, labelFill, bufferColor,
+                          bufferWidth, placement);
+
+    return style;
+};
